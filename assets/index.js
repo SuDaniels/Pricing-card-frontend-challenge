63a4e1f717275e91cac3c36744f8195e7a267054
SuDaniels <oluwaseundaniels17@gmail.com>
2024-02-11 17:56:34 +0100

    Updated

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,22 @@
-function alertUser() {
-  alert("Sign in successful!");
+// function alertUser() {
+//   alert("Sign in successful!");
+// }
+// function NotResponsive() {
+//   alert("Applications are closed for now, please try again next symester!");
+// }
+
+function validateSignin() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const predefinedUsername = "admin"; //
+  const predefinedPassword = "admin123";
+
+  if (username === predefinedUsername && password === predefinedPassword) {
+    document.getElementById("result").innerHTML = "Sign in successful!";
+  } else {
+    document.getElementById("result").innerHTML =
+      "Invalid username or password.";
+  }
 }
-function NotResponsive() {
-  alert("Applications are closed for now, please try again next symester!");
-}
+
+// validateSignin();
